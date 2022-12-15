c036715509efa209d7a2b2fe5999f5939f4f753e
André Lopes <andrelneres@hotmail.com>
2022-12-15 15:57:26 -0300

    Carregando as Relações

--- Controllers/PedidoController.cs
+++ Controllers/PedidoController.cs
@@ -27,5 +27,19 @@ namespace sistema_vendas_ti_adacemy.Controllers
             _repository.Cadastrar(pedido);
             return Ok(pedido);
         }
+
+        [HttpGet("{id}")]
+        public IActionResult ObterPorId(int id)
+        {
+            var pedido = _repository.ObterPorId(id);
+
+            if (pedido is not null)
+            {
+                return Ok(pedido);
+            }
+            else
+                return NotFound(new { Mensagem = "Pedido não encontrado" });
+        }
+
     }
 }
--- Repository/PedidoRepository.cs
+++ Repository/PedidoRepository.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
 using sistema_vendas_ti_adacemy.Context;
 using sistema_vendas_ti_adacemy.Models;
 
@@ -20,6 +21,14 @@ namespace sistema_vendas_ti_adacemy.Repository
         {
             _context.Pedidos.Add(pedido);
             _context.SaveChanges();
+            return pedido;
+        }
+
+        public Pedido ObterPorId(int id)
+        {
+            var pedido = _context.Pedidos.Include(x => x.Vendedor)
+                    .Include(x => x.Cliente)
+                    .FirstOrDefault(x => x.Id == id);
             return pedido;
         }
     }
